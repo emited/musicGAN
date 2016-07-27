b2bf63c9544c0c34d2510a6a7d755c08a40cf1f4
emited <emmanueldebezenac@gmail.com>
2016-07-28 00:03:00 +0200

    canges

--- scripts/cwt_signal.py
+++ scripts/cwt_signal.py
@@ -6,7 +6,7 @@ from scipy.io import wavfile
 sigrate, sig = wavfile.read('../data/voice.wav')
 print(sigrate, len(sig))
 
-widths = np.arange(1, 1001)
+widths = np.arange(1,31)
 
 cwtmatr = signal.cwt(sig, signal.ricker, widths)
 plt.imshow(cwtmatr, extent=[-1, 1, 1, 31], cmap='PRGn', aspect='auto',vmax=abs(cwtmatr).max(), vmin=-abs(cwtmatr).max())
